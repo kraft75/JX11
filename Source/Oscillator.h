3687d335ade9e8371198530fa848a82ccc16dff8
jay <iuuh@hotmail.de>
2024-05-04 19:01:02 +0200

    Changed sine oscillator to a plain sawtooth oscillator

--- Source/Oscillator.h
+++ Source/Oscillator.h
@@ -24,15 +24,27 @@ public:
     float inc;
     float phase;
     
+//    Variant digital resonator.
+    float sin0;
+    float sin1;
+    float dsin;
+    
     void reset()
     {
 //        Cosine wave
         phase = 0.0f;
+        
+        //    Variant digital resonator.
+        sin0 = amplitude * std::sin(phase * TWO_PI);
+        sin1 = amplitude * std::sin((phase - inc) * TWO_PI);
+        dsin = 2.0f * std::cos(inc * TWO_PI);
+
     }
     
 //    Outputs audio data (sine tone)
     float nextSample()
     {
+        /*
         phase += inc;
 //        It takes sampleRate/freq samples for phase to count from 0 to 1.
         if (phase >= 1.0f) {
@@ -41,5 +53,12 @@ public:
 //        Sine needs as an argument an angle in radians
 //        phase shows the current position of sine
         return amplitude * std::sin(TWO_PI * phase);
+         */
+        
+//            Variant digital resonator.
+//            An alternative that saves computing time by the sine function
+        float sinx = dsin * sin0 - sin1; sin1 = sin0;
+        sin0 = sinx;
+        return sinx;
     }
 };
